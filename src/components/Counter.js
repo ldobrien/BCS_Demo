73dd35341160294398e9023c5a4a356af4acc8f9
Lisa <lisaobrien2009@hotmail.com>
2020-12-08 15:08:42 -0500

    Added an API Call

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Counter extends Component {
   // create a state variable for counting
     state = {
-      counter: 0
+      counter: 0,
+      dog: null,
     }
 
     onClick = () => {
       this.setState({
         counter: this.state.counter + 1
       })
-      // console.log("Clicked a button")
+    }
+
+    onClickPuppy = () => {
+      axios.get(`https://dog.ceo/api/breeds/image/random`)
+      .then(res => {
+        console.log(res)
+        const dog = res.data.message;
+        this.setState({ dog });
+      })
     }
 
     render() {
       return <div>
         <p>{this.state.counter}</p>
         <button onClick={this.onClick}>Increase</button>
+        <div>
+          <button onClick={this.onClickPuppy}>Get me a Puppy</button>
+        </div>
+          <img src={this.state.dog} />
+        
+        
       </div>
     }
 }

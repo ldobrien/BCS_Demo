1144f9f906ad3a64caa5d41f798ada9de73da692
Lisa <lisaobrien2009@hotmail.com>
2020-12-10 15:33:53 -0500

    added materialize and basic styling

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,69 +1,82 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import DogDetails from './DogDetails'
-import './Counter.css'
+import React, { Component } from "react";
+import axios from "axios";
+import DogDetails from "./DogDetails";
+import { Icon, Button } from "react-materialize";
+import "./Counter.css";
 
 class Counter extends Component {
   // create a state variable for counting
-    state = {
-      counter: 0,
-      dog: [],
-      dogNames: []
-    }
+  state = {
+    counter: 0,
+    dog: [],
+    dogNames: [],
+  };
 
-    onClick = () => {
-      this.setState({
-        counter: this.state.counter + 1
-      })
-    }
+  onClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
 
-    onClickPuppy = () => {
-      const requestOne = axios.get(`https://dog.ceo/api/breeds/image/random`)
-      const requestTwo = axios.get(`https://randomuser.me/api/`)
+  onClickPuppy = () => {
+    const requestOne = axios.get(`https://dog.ceo/api/breeds/image/random`);
+    const requestTwo = axios.get(`https://randomuser.me/api/`);
 
-      axios.all([requestOne, requestTwo])
-      .then(axios.spread((...responses) => {
-        const responseOne = responses[0]
-        const responseTwo = responses[1]
-        const dog = responseOne.data.message
-        const dogNames = responseTwo.data.results[0].name
-        const dogName = dogNames.title + " " + dogNames.first + " " + dogNames.last
+    axios.all([requestOne, requestTwo]).then(
+      axios.spread((...responses) => {
+        const responseOne = responses[0];
+        const responseTwo = responses[1];
+        const dog = responseOne.data.message;
+        const dogNames = responseTwo.data.results[0].name;
+        const dogName =
+          dogNames.title + " " + dogNames.first + " " + dogNames.last;
         this.setState({
           dog: [...this.state.dog, dog],
-          dogNames: [...this.state.dogNames, dogName]
-        })
-      }
-      ))
-      // axios.get(`https://dog.ceo/api/breeds/image/random`)
-      // .then(res => {
-      //   const dog = res.data.message;
-      //   this.setState({ dog: [...this.state.dog, dog] });
-      // })
-      // axios.get(`https://randomuser.me/api/`)
-      // .then(res => {
-      //   const dogName = res.data.results[0].name.title + res.data.results[0].name.first + res.data.results[0].name.last
-      //   this.setState({ dogNames: [...this.state.dogNames, dogName]})
-      // })
-    }
-
-    render() {
-      console.log(this.state.dog)
-      let dogPhotos = []
-      this.state.dog.forEach((dogPhoto, index) => {
-        dogPhotos.push(<DogDetails name={this.state.dogNames[index]} photo={dogPhoto}/>)
+          dogNames: [...this.state.dogNames, dogName],
+        });
       })
-      return <div className="container">
-        <div className="limegreen">
-          <p>{this.state.counter}</p>
-          <button onClick={this.onClick}>Increase</button>
+    );
+  };
+
+  render() {
+    let dogPhotos = [];
+    this.state.dog.forEach((dogPhoto, index) => {
+      dogPhotos.push(
+        <div className="col s3">
+          <DogDetails name={this.state.dogNames[index]} photo={dogPhoto} />
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="white center card">
+          <div className="blue-text counter">{this.state.counter}</div>
+          <Button
+            className="blue"
+            floating
+            icon={<Icon>+</Icon>}
+            large
+            node="button"
+            waves="light"
+            onClick={this.onClick}
+          />
         </div>
 
-        <div>
-          <button onClick={this.onClickPuppy}>More Dogs Please...</button>
+        <div className="center">
+          <Button
+            className="blue"
+            large
+            node="button"
+            waves="light"
+            onClick={this.onClickPuppy}
+          >
+            More dogs Please...
+          </Button>
         </div>
-        {dogPhotos}
+        <div className="row">{dogPhotos}</div>
       </div>
-    }
+    );
+  }
 }
 
 export default Counter;
--- src/components/DogDetails.js
+++ src/components/DogDetails.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
-import './Counter.css'
+import React, { Component } from "react";
 
 class DogDetails extends Component {
-    render() {
-      return <div className="sidebyside" >
-          <p>{this.props.name}</p>
-        <img className="image" src={this.props.photo} />
+  render() {
+    return (
+      <div class="card">
+        <div class="card-image">
+          <img className="image" src={this.props.photo} />
+          <span class="card-title">{this.props.name}</span>
+        </div>
       </div>
-    }
+    );
+  }
 }
 
 export default DogDetails;
